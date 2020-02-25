84f5c31d422b7653655120112ed04001ca8fea2b
Adrian Corsini <glenflesk@gmail.com>
2020-02-25 23:11:38 +0000

    cleaned up the test file

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
 class NewVisitorText(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -11,30 +13,44 @@ class NewVisitorText(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        #Edith has heard about a cool new online to-do app. She goes
-        #to check it out its homepate
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check it out its homepate
         self.browser.get('http://localhost:8000')
         # She notices the path title and header mention to-do lists
-        self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        # She types "Buy peacock feathers" into a text box(Ediths hobby is 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # She types "Buy peacock feathers" into a text box(Ediths hobby is
         #  is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        #When she hits enter, the page updaes, and now the page lists
+        # When she hits enter, the page updaes, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-d0 list
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peakcock feathers' for row in rows)
+        )
         # There is still a text box inviting to add another item. She
         # enters "Use peacock feathers to make a fly"(Edith is very meth
+        self.fail('Finish the test!')
         # The page updates again, and now shows both items on her list
 
         # Edith wonders whether the site will remember her list. Then she
-        # that the site has generated a unique URL for her -- there is so 
+        # that the site has generated a unique URL for her -- there is so
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there
 
         # Satisfied, she goes back to sleep
 if __name__ == '__main__':
-    unittest.main(warnings = 'ignore')
+    unittest.main(warnings='ignore')
